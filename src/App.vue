00c3e06f6bf08be634df69d2c88da84be3ee59ad
JunJieLin1s <junjie@vitrion.nl>
2025-05-16 11:33:35 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Navbar from './components/Navbar.vue';
+</script>
+
+<template>
+  <Navbar />
+  <header>
+   
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
